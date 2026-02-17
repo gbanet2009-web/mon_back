758cabab390df9afc6f10c8afbfaf3aa7820afcd
gbane2009-web <gbanet2009@gmail.com>
2026-02-17 12:06:48 +0000

    full prod

--- my_backend_project/urls.py
+++ my_backend_project/urls.py
@@ -1,23 +1,17 @@
-"""
-URL configuration for my_backend_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include # Import include
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,   # login → obtient access + refresh
+    TokenRefreshView       # refresh token
+)
+from . import views  # tes vues api existantes
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), # Include API app URLs
+    # JWT Auth
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Tes endpoints API
+    path('courses/', views.CourseListView.as_view(), name='courses'),
+    path('exercises/', views.ExerciseListView.as_view(), name='exercises'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
