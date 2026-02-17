84ef3f2e2d14aedc1e5534161f50b73f8b23903e
gbanet2009-web! <gbanet2009@gmail.com>
2026-02-17 14:53:35 +0000

    pouref

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
+from .views import CourseListView, ExerciseListView, UserProfileView, LoginView, UpdateClassView, AddCreditsView, BacSubjectListView
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('profile/', views.UserProfileView.as_view(), name='profile'),
-    path('update-class/', views.UpdateClassView.as_view(), name='update_class'),
-    path('add-credits/', views.AddCreditsView.as_view(), name='add_credits'),
-    path('courses/', views.CourseListView.as_view(), name='courses'),
-    path('exercises/', views.ExerciseListView.as_view(), name='exercises'),
-    path('bac-subjects/', views.BacSubjectListView.as_view(), name='bac_subjects'),
+    path('courses/', CourseListView.as_view(), name='courses'),
+    path('exercises/', ExerciseListView.as_view(), name='exercises'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('update_class/', UpdateClassView.as_view(), name='update_class'),
+    path('add_credits/', AddCreditsView.as_view(), name='add_credits'),
+    path('bac_subjects/', BacSubjectListView.as_view(), name='bac_subjects'),
 ]
--- my_backend_project/urls.py
+++ my_backend_project/urls.py
@@ -1,18 +1,13 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-from api.views import CourseListView, ExerciseListView, UserProfileView
-
 urlpatterns = [
-    # JWT
+    path('admin/', admin.site.urls),  # Admin site
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # API
-    path('courses/', CourseListView.as_view(), name='courses'),
-    path('exercises/', ExerciseListView.as_view(), name='exercises'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('api/', include('api.urls')),  # API app URLs
 ]
